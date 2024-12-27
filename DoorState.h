34bf3d676246ebc9d3178121a6f95f9ac66a0ea9
naylom <mark@naylorfamily.org>
2024-12-27 15:39:27 +0000

    fix relay logical values with el817

--- DoorState.h
+++ DoorState.h
@@ -34,8 +34,8 @@ constexpr RGBType	DOOR_BAD_COLOUR			  = MNRGBLEDBaseLib::DARK_YELLOW;
 constexpr RGBType	DOOR_UNKNOWN_COLOUR		  = MNRGBLEDBaseLib::BLUE;
 constexpr uint8_t	DOOR_STATIONARY_FLASHTIME = 0;
 constexpr uint8_t	DOOR_MOVING_FLASHTIME	  = 10; // 20 = 1 sec
-constexpr PinStatus RELAY_ON				  = LOW;
-constexpr PinStatus RELAY_OFF				  = HIGH;
+constexpr PinStatus RELAY_ON				  = HIGH;
+constexpr PinStatus RELAY_OFF				  = LOW;
 
 class DoorStatusPin;
 class DoorStatusCalc;
